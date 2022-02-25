1bff73039a2f400c407223c6079c382ad095922b
Advaita Saha <adv11sphs@gmail.com>
2022-02-25 21:38:39 +0530

    lottery app completed

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,84 @@
+import web3 from "./web3";
+
+const address = "0x7AF5A5E3bbF646d0e2A121FcDCeE9f031AF28aAB";
+const abi = [
+	{
+		"inputs": [],
+		"name": "enter",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "pickWinner",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "manager",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "players",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "playersData",
+		"outputs": [
+			{
+				"internalType": "address[]",
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "random",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+export default new web3.eth.Contract(abi, address);
--- lottery/deploy.js
+++ lottery/deploy.js
@@ -3,11 +3,13 @@ const Web3 = require('web3');
 const dotenv = require('dotenv');
 const { abi, evm } = require('./compile');
 
+
 dotenv.config();
 const provider = new HDWalletProvider(
     process.env.SEED_PHRASE,
     'https://rinkeby.infura.io/v3/42b22014fc704adea44d3e65731592ba',
 );
+
 
 const web3 = new Web3(provider);
 
@@ -20,6 +22,7 @@ const deploy = async () => {
         .deploy({ data : evm.bytecode.object })
         .send({ gas : '1000000', from : accounts[0] });
 
+    console.log(abi);
     console.log('Contract deployed to', result.options.address);
     provider.engine.stop();
 };
